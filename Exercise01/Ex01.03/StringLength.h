fd704fac428c3b2c0cf3c2a048ed8e89a7896d37
Jonah Lawrence <jonah@freshidea.com>
2021-08-10 00:10:42 +0300

    01.03 String length exercise

---
+++ Exercise01/Ex01.03/StringLength.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <stdio.h>
+
+/**
+ * @file String Length
+ */
+
+ /**
+  * @brief Returns the length of the given
+  * null-terminated byte string, that is,
+  * the number of characters that precede
+  * the terminating null character.
+  *
+  * @param input The string to be measured
+  * @return size_t The length of the string
+  */
+size_t StringLength(const char *input);
